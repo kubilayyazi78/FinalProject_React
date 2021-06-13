e2b2c0a54b36f732c21c88a3e42db513d3b0891e
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-06-13 20:49:15 +0300

    Router yapısı kuruldu.Detay sayfası olusturuldu.

--- camp-project/src/layouts/Navi.jsx
+++ camp-project/src/layouts/Navi.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Container, Menu } from "semantic-ui-react";
 import CartSummary from "./CartSummary";
 import SignedIn from "./SignedIn";
@@ -6,9 +7,10 @@ import SignedOut from "./SignedOut";
 
 export default function Navi() {
   const [isAhuthenticated, setIsAhuthenticated] = useState(true);
-
+  const history =useHistory();
   function handleSignOut(params) {
     setIsAhuthenticated(false);
+    history.push("/");
   }
   function handleSignIn(params) {
     setIsAhuthenticated(true);
